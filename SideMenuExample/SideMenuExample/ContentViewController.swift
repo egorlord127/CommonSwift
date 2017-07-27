e11032a3a59ac7422b9e9bee108e2684d0bdce61
egorlord <egor.lord-127@yandex.com>
2017-07-26 18:23:23 -0700

    basic

---
+++ SideMenuExample/SideMenuExample/ContentViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ContentViewController.swift
+//  SideMenuExample
+//
+//  Created by Egor Lord on 7/26/17.
+//  Copyright © 2017 Egor Lord. All rights reserved.
+//
+
+import UIKit
+import SideMenuController
+
+class ContentViewController: UIViewController, SideMenuControllerDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.barTintColor = randomColor
+        sideMenuController?.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var randomColor: UIColor {
+        let colors = [UIColor(hue:0.65, saturation:0.33, brightness:0.82, alpha:1.00),
+                      UIColor(hue:0.57, saturation:0.04, brightness:0.89, alpha:1.00),
+                      UIColor(hue:0.55, saturation:0.35, brightness:1.00, alpha:1.00),
+                      UIColor(hue:0.38, saturation:0.09, brightness:0.84, alpha:1.00)]
+        
+        let index = Int(arc4random_uniform(UInt32(colors.count)))
+        return colors[index]
+    }
+    
+    @IBAction func presentAction() {
+        present(ViewController.fromStoryboard, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    func sideMenuControllerDidHide(_ sideMenuController: SideMenuController) {
+        print(#function)
+    }
+    
+    func sideMenuControllerDidReveal(_ sideMenuController: SideMenuController) {
+        print(#function)
+    }
+}
--- SideMenuExample/SideMenuExample/ViewController.swift
+++ SideMenuExample/SideMenuExample/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    static var fromStoryboard: ViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as! ViewController
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
