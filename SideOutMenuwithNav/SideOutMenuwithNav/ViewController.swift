95a171eccd14a297f14f7ec175b0734468f62cea
egorlord <egor.lord-127@yandex.com>
2017-07-29 09:57:57 -0700

    basic sidemenu

--- SideOutMenuwithNav/SideOutMenuwithNav/ViewController.swift
+++ SideOutMenuwithNav/SideOutMenuwithNav/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftCon: NSLayoutConstraint!
+    @IBOutlet weak var leftContainer: UIView!
+    var showingMenu: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        leftCon.constant = -leftContainer.frame.size.width
+//        self.view.layoutIfNeeded()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showMenu(_ sender: Any) {
+        
+        if(leftCon.constant != 0)
+        {
+            leftCon.constant = 0
+            UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded()})
+        } else {
+            leftCon.constant = -leftContainer.frame.size.width
+            UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded()})
+        }
+    }
 
 }
 
